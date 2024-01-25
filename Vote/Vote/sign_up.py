19f66cbf5259f9e32fe9edd1b221bf2b0cb73692
ovibaridar <adgjmp1414@>
2024-01-25 17:42:29 +0600

    update

--- Vote/Vote/sign_up.py
+++ Vote/Vote/sign_up.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def sign_up(request):
     return render(request, 'Sign_up_.html')
+
+
+def sign_up_data_collect(request):
+    if request.method == 'POST':
+        return redirect('login')
--- Vote/Vote/urls.py
+++ Vote/Vote/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='Home'),
     path('sign_up', sign_up.sign_up, name='sign_up'),
+    path('sign_up_data_collect', sign_up.sign_up_data_collect, name='sign_up_data_collect'),
     path('login', login.login, name='login'),
     path('create_poll', creat_poll.creat_poll, name='create_poll'),
     path('results', results.results, name='results'),
